b8a8c48b7c48a6b11dbb3df16f9d4662752d2276
prachi-mujumdar <prachi.mujumdar@mastek.com>
2016-02-18 15:01:57 -0500

    US12 : TA15 status : In-Progress

--- src/main/java/com/blogpost/helloworldmaven/HelloWorldResource.java
+++ src/main/java/com/blogpost/helloworldmaven/HelloWorldResource.java
@@ -68,7 +68,7 @@ public class HelloWorldResource {
      * Retrieves representation of an instance of helloworld.HelloWorldResource
      *
      * @return an instance of java.lang.String
-	 Updated
+	 Updated 1
      */
     @GET
     @Produces("text/html")
